9fa711cef333edcfc31ee31d6e6e280ab27588c7
pepijn03 <pepijnvanloenhout@gmail.com>
2022-09-19 14:21:48 +0200

    first try communication with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import  axios from "axios";
+
+const Film = () => {
+  const FetchFilm = () => {
+    axios.get("http://localhost:8080/api/v1/film").then(( res) => {})
+
+    console.log(res);
+  }
+
+  useEffect(() => {
+    FetchFilm();
+  }, [])
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Film/>
     </div>
   );
 }
