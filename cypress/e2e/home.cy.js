5cc1e73c9f202db36bcc1943ebd7d46c9b42e9f2
pepijn03 <pepijnvanloenhout@gmail.com>
2022-12-20 18:53:04 +0100

    update cypress test

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,13 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('displays films', () => {
+    cy.get('.MuiGrid-root div').should('have.length.greaterThan', 0)
+  })
+})
